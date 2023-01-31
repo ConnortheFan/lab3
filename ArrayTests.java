63ff0c40557fb3e8eba9534115fa1476d265acad
Connor Fan <ConnortheFan@gmail.com>
2023-01-30 21:49:24 -0800

    ArrayExamples MyTests

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,26 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+    @Test
+    public void testMyReverseInPlace() {
+        int[] input1 = {1,2,3,4,5};
+        ArrayExamples.reverseInPlace(input1);
+        assertArrayEquals(new int[]{5,4,3,2,1}, input1);
+    }
+    
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testMyReversed() {
+      int[] input1 = {1,2,3,4,5};
+      int[] input1Rev = ArrayExamples.reversed(input1);
+      assertArrayEquals(new int[]{5,4,3,2,1}, input1Rev);
+  }
+ 
 }
+
